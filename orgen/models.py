a93ba46f7725df7c5dfa10322d748593643d8e25
sohailaebrahime66 <sohailaebrahimhassan@gmail.com>
2026-06-18 12:57:49 +0300

    Update models.py

--- orgen/models.py
+++ orgen/models.py
@@ -8,7 +8,6 @@ from django.utils import timezone
 import datetime
 from django.urls import reverse
 from django.conf import settings
-from cloudinary_storage.storage import MediaCloudinaryStorage  
 
 
 
@@ -582,9 +581,9 @@ class Surgery(models.Model):
 # MRI Reports
 class MRIReport(models.Model):
     patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mri_reports')
-    before_scan = models.FileField(upload_to='mri/before/', null=True, blank=True , storage=MediaCloudinaryStorage(tag='public'))
-    after_scan = models.FileField(upload_to='mri/after/', null=True, blank=True ,storage=MediaCloudinaryStorage(tag='public'))
-    npl_report = models.FileField(upload_to='mri/npl/', null=True, blank=True ,storage=MediaCloudinaryStorage(tag='public'))
+    before_scan = models.FileField(upload_to='mri/before/', null=True, blank=True )
+    after_scan = models.FileField(upload_to='mri/after/', null=True, blank=True )
+    npl_report = models.FileField(upload_to='mri/npl/', null=True, blank=True)
     ai_result = models.TextField(null=True, blank=True)
     mismatch_alert = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -683,7 +682,7 @@ class UserReport(models.Model):
 
     patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reports')
     report_type = models.CharField(max_length=50, choices=type)
-    report_file = models.FileField(upload_to='user_reports/', null=True, blank=True ,storage =MediaCloudinaryStorage(tag='public'))
+    report_file = models.FileField(upload_to='user_reports/', null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     report_title = models.TextField(null=True, blank=True)
     state = models.CharField(max_length=20, choices=reportState, null=False)
@@ -701,8 +700,8 @@ class SurgeryReport(models.Model):
     complications = models.TextField(null=True, blank=True)
     doctor_notes = models.TextField(null=True, blank=True)
     recorded_at = models.DateTimeField(auto_now_add=True)
-    report_file = models.FileField(upload_to='surgery_reports/files/', null=True, blank=True ,storage=MediaCloudinaryStorage(tag='public'))
-    report_image = models.FileField(upload_to='surgery_reports/images/', null=True, blank=True ,storage=MediaCloudinaryStorage  (tag='public'))
+    report_file = models.FileField(upload_to='surgery_reports/files/', null=True, blank=True )
+    report_image = models.FileField(upload_to='surgery_reports/images/', null=True, blank=True )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
